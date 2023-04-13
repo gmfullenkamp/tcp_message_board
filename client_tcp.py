72928cb5ec8b26df8d888f71431619e9820e0240
gmfullenkamp <gmfullenkamp@gmail.com>
2023-04-13 12:02:23 -0400

    Added comments to the client code.

--- client_tcp.py
+++ client_tcp.py
@@ -4,11 +4,15 @@ HOST = 'localhost'  # the server's hostname or IP address
 PORT = 8888        # the port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connect the client to the server
     s.connect((HOST, PORT))
     while True:
+        # Allow the user to input any command/message whenever they want
         message = input("Type a message to send to the server: ")
         s.sendall(message.encode('utf-8'))
+        # Make the user listen for the server response
         data = s.recv(1024)
         while not data:
             data = s.recv(1024)
+        # Ouput the server response
         print('Server sent:', data.decode('utf-8'))
