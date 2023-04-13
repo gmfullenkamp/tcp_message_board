032c15fa9a3d68b9dde31786844c2d2482ef171b
gmfullenkamp <gmfullenkamp@gmail.com>
2023-04-13 10:16:04 -0400

    Updated the client to be allowed to send as many messages as they want, and updated the server to recieve multiple messages from a single client.

--- client_tcp.py
+++ client_tcp.py
@@ -5,5 +5,6 @@ PORT = 8888        # the port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    message = input("Type a message to send to the server: ")
-    s.sendall(message.encode('utf-8'))
+    while True:
+        message = input("Type a message to send to the server: ")
+        s.sendall(message.encode('utf-8'))
--- server_tcp.py
+++ server_tcp.py
@@ -1,16 +1,35 @@
 import socket
+import select
 
-HOST = 'localhost'  # the server's hostname or IP address
-PORT = 8888        # the port used by the server
+HOST = 'localhost'
+PORT = 8888
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server listening on port {PORT}")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print(f'Server listening on port {PORT}')
+    sockets_list = [server_socket]
+    clients = {}
+
     while True:
-        conn, addr = s.accept()
-        print(f"Client {addr[0]}:{addr[1]} has connected")
-        with conn:
-            data = conn.recv(1024)
-            if data:
-                print(f"Client {addr[0]}:{addr[1]} sent: {data}")
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+        for notified_socket in read_sockets:
+            # TODO: Fix to allow multiple clients to send/recieve simultaneously
+            if notified_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                print(f'New client connected: {client_address[0]}:{client_address[1]}')
+                sockets_list.append(client_socket)
+                clients[client_socket] = client_address
+            else:
+                message = notified_socket.recv(1024)
+                if not message:
+                    print(f'Client disconnected: {clients[notified_socket][0]}:{clients[notified_socket][1]}')
+                    sockets_list.remove(notified_socket)
+                    del clients[notified_socket]
+                    continue
+                print(f'Received message from {clients[notified_socket][0]}:{clients[notified_socket][1]}: {message.decode()}')
+                for client_socket in clients:
+                    if client_socket != server_socket and client_socket != notified_socket:
+                        client_socket.send(message)
