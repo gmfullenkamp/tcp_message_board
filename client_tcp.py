aa358b561d80aac98698724e7efeefa515774ee8
gmfullenkamp <gmfullenkamp@gmail.com>
2023-04-13 11:56:23 -0400

    Got the server able to send messages to the clients. client <---> server

--- client_tcp.py
+++ client_tcp.py
@@ -8,3 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         message = input("Type a message to send to the server: ")
         s.sendall(message.encode('utf-8'))
+        data = s.recv(1024)
+        while not data:
+            data = s.recv(1024)
+        print('Server sent:', data.decode('utf-8'))
