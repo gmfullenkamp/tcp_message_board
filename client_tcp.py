1144914f89d8eee69bac59aaebbe858e8da51318
tdarst <trevordarst@gmail.com>
2023-04-14 16:55:47 -0400

    Finished all commands except for post and messages related and did some error handling as well (it's commented where it's implemented

--- client_tcp.py
+++ client_tcp.py
@@ -4,28 +4,35 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Wait for the user to %connect to their desired server
     print("Welcome to Chat GTC, the Grant and Trevor Client.\n"
-          "Please use the '%connect <host_addr:port_#> <username>' to connect to your desired server.\n")
+          "Please use the command '%connect <host_addr:port_#> <username>' to connect to your desired server.\n")
     while True:
-        connect_command = input()
-        if "%connect " in connect_command:
-            host_addr, username = connect_command.split(" ")[1:3]
-            print(f"Connecting to {host_addr} as user {username}...")
-            host, port = host_addr.split(":")
-            # Connect the client to the server
-            s.connect((host, int(port)))
-            # Allow the user to input any command/message whenever they want
-            s.sendall(username.encode('utf-8'))
-            break
-        else:
-            print("Try again.")  # TODO: Error Handling
+        while True:
+            connect_command = input()
+            if "%connect " in connect_command:
+                host_addr, username = connect_command.split(" ")[1:3]
+                print(f"Connecting to {host_addr} as user {username}.")
+                host, port = host_addr.split(":")
+                # Connect the client to the server
+                s.connect((host, int(port)))
+                # Allow the user to input any command/message whenever they want
+                s.sendall(username.encode('utf-8'))
+                # Make the user listen for the server response
+                data = s.recv(1024)
+                # Output the server response
+                print('Server sent:', data.decode('utf-8'))
+                break
+            else:
+                print("Try again.")  # TODO: Error Handling
 
-    while True:
-        # Allow the user to input any command/message whenever they want
-        message = input("Type a message to send to the server: ")
-        s.sendall(message.encode('utf-8'))
-        # Make the user listen for the server response
-        data = s.recv(1024)
-        while not data:
+        while True:
+            # Allow the user to input any command/message whenever they want
+            message = input("Type a command to send to the server: ")
+            s.sendall(message.encode('utf-8'))
+            # Make the user listen for the server response
             data = s.recv(1024)
-        # Output the server response
-        print('Server sent:', data.decode('utf-8'))
+            while not data:
+                data = s.recv(1024)
+            # Output the server response
+            print('Server sent:', data.decode('utf-8'))
+            if data == b"Disconnected from server.":  # If the user exits, they return to the start
+                break
--- server_tcp.py
+++ server_tcp.py
@@ -2,12 +2,12 @@ import socket
 import select
 
 HOST = 'localhost'  # the server's hostname or IP address
-PORT = 8888        # the port used by the server
+PORT = 8888         # the port used by the server
 
+group_list = []
 
-groupList = []
+user_list = []
 
-userList = []
 
 class Group:
     def __init__(self, name):
@@ -15,13 +15,17 @@ class Group:
         self.users = []
         self.recent = []
 
+
 class User:
-    def __init__(self, username, portNumber):
-        self.username = username
-        self.portNumber = portNumber
+    def __init__(self, name, port_number):
+        self.name = name
+        self.port_number = port_number
         self.group = None
 
+
 publicGroup = Group("public")
+
+group_list.append(publicGroup)
 
 # Create the server socket and bind it to the specified host and port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -51,35 +55,133 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                 sockets_list.append(client_socket)
                 clients[client_socket] = client_address
 
-                #boolean to mark user's first connection to create user object from entered username
+                # Boolean to mark user's first connection to create user object from entered username
                 first = True
 
             else:
                 # Receive data from the client
                 message = notified_socket.recv(1024)
-                print(message)
+
+                # If the client has sent a message, print it and send it to all connected clients
+                print(f'Received command from {clients[notified_socket][0]}:{clients[notified_socket][1]}: '
+                      f'{message.decode()}')
+
+                # If first time user has connected, get username and port number to update User object and userList
+                if first:
+                    user_list.append(User(message.decode(), clients[notified_socket][1]))
+                    print(user_list[-1].name, user_list[-1].port_number)
+                    first = False
+                    notified_socket.sendall(message + b' is connected.')
                     
-                # If the client has disconnected, remove the socket from the list and the dictionary
-                if not message:
-                    print(f'Client disconnected: {clients[notified_socket][0]}:{clients[notified_socket][1]}')
+                elif b'%exit' == message.split(b' ')[0]:  # Exit command removes the client
+                    print(f'Client exiting: {clients[notified_socket][0]}:{clients[notified_socket][1]}')
+                    for user in user_list:
+                        if user.port_number == clients[notified_socket][1]:
+                            break
+                    user_list.remove(user)
+                    notified_socket.sendall(b'Disconnected from server.')
                     sockets_list.remove(notified_socket)
                     del clients[notified_socket]
                     continue
-
-                # If the client has sent a message, print it and send it to all connected clients
-                print(f'Received message from {clients[notified_socket][0]}:{clients[notified_socket][1]}: {message.decode()}')
-                notified_socket.sendall(b'OK')
-                for client_socket in clients:
-                    if client_socket != server_socket and client_socket != notified_socket:
-                        client_socket.send(message)
-
-                #if first time user has connect get username and port number to update User object and userList
-                if first:
-                    userList.append(User(message.decode(), clients[notified_socket][1]))
-                    print(userList[-1].username, userList[-1].portNumber)
-                    first = False
-
-        # Handle sockets that have exceptions (e.g. a client has disconnected unexpectedly)
-        for notified_socket in exception_sockets:
-            sockets_list.remove(notified_socket)
-            del clients[notified_socket]
+                
+                elif b'%join' == message.split(b' ')[0]:
+                    for user in user_list:
+                        if user.port_number == clients[notified_socket][1]:
+                            #Error handling implemented if user is already in public
+                            if user.group != 'public':
+                                group_list[0].users.append(user)
+                                user.group = 'public'
+                                break
+                            else:
+                                notified_socket.sendall(b'User is already in public')
+                                continue
+                            
+                    notified_socket.sendall(b'User added to public')
+                    continue
+                
+                elif b'%post' == message.split(b' ')[0]:  # TODO: Post command
+                    notified_socket.sendall(b'%post command is not implemented.')
+                    
+                elif b'%users' == message.split(b' ')[0]:
+                    send_string = 'List of connected users: \n'
+                    for user in user_list:
+                        send_string += user.name + '\n'
+                    notified_socket.sendall(send_string.encode())
+                    continue
+                
+                elif b'%leave' == message.split(b' ')[0]:
+                    for user in user_list:
+                        if user.port_number == clients[notified_socket][1]:
+                            #Error handling implemented if user is not in public
+                            if user.group == 'public':
+                                group_list[0].users.remove(user)
+                                user.group = None
+                                break
+                            else:
+                                notified_socket.sendall(b'User is not in public')
+                                continue
+                    notified_socket.sendall(b'User removed from public')
+                    continue
+                
+                elif b'%message' == message.split(b' ')[0]:  # TODO: Message command
+                    notified_socket.sendall(b'%message command is not implemented.')
+                    
+                elif b'%groups' == message.split(b' ')[0]:
+                    send_string = 'List of groups: \n'
+                    for group in group_list:
+                        send_string += group.name + '\n'
+                    notified_socket.sendall(send_string.encode())
+                    continue
+                
+                elif b'%groupjoin' == message.split(b' ')[0]:
+                    for user in user_list:
+                        if user.port_number == clients[notified_socket][1]:
+                            for group in group_list:
+                                if message.split(b' ')[1].decode() == group.name:
+                                    #Error handling implemented for if user is already in requested group
+                                    if user.group != group.name:
+                                        group.users.append(user)
+                                        user.group = group.name
+                                        break
+                                    else:
+                                        print(group.users)
+                                        notified_socket.sendall(b'User is already in ' + group.name.encode())
+                                        continue
+                                    
+                    notified_socket.sendall(b'User added to ' + group.name.encode())
+                    continue
+                
+                elif b'%grouppost' == message.split(b' ')[0]:  # TODO: Grouppost command
+                    notified_socket.sendall(b'%grouppost command is not implemented.')
+                    
+                elif b'%groupusers' == message.split(b' ')[0]:
+                    send_string = 'List of users in ' + message.split(b' ')[1].decode() + ':\n'
+                    for group in group_list:
+                        if message.split(b' ')[1].decode() == group.name:
+                            for user in group.users:
+                                send_string += user.name + '\n'
+                    notified_socket.sendall(send_string.encode())
+                    continue
+                    
+                elif b'%groupleave' == message.split(b' ')[0]:
+                    for user in user_list:
+                        if user.port_number == clients[notified_socket][1]:
+                            for group in group_list:
+                                if message.split(b' ')[1].decode() == group.name:
+                                    #Error handling implemented for if user is not in requested group
+                                    if user.group == group.name:
+                                        group.users.remove(user)
+                                        user.group = None
+                                        break
+                                    else:
+                                        notified_socket.sendall(b'User is not in ' + group.name.encode())
+                                        continue
+                                    
+                    notified_socket.sendall(b'User removed from ' + group.name.encode())
+                    continue
+                
+                elif b'%groupmessage' == message.split(b' ')[0]:  # TODO: Groupmessage command
+                    notified_socket.sendall(b'%groupmessage command is not implemented.')
+                    
+                else:  # TODO: Remove else in future.
+                    notified_socket.sendall(message)
